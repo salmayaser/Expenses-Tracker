a71605dc187970f28d2db4edfdc4aa1c4bf1a5b2
salmayaser <salmayasser161@gmail.com>
2022-06-05 14:00:42 +0200

    Finalizing Expenses tracker app

--- src/components/Filter/ExpenseFilter.js
+++ src/components/Filter/ExpenseFilter.js
@@ -8,11 +8,9 @@ const ExpensesFilter = (props) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={props.filter} onChange={filterChangeHandler}>
+        <select defaultValue={props.filter} onChange={filterChangeHandler}>
           <option value="2022">2022</option>
-          <option selected value="2021">
-            2021
-          </option>
+          <option value="2021">2021</option>
           <option value="2020">2020</option>
           <option value="2019">2019</option>
         </select>
