1dad84b9db6b08b0cb4246a5a554755a566ae597
salmayaser <salmayasser161@gmail.com>
2022-11-07 15:27:45 +0200

    Day 2

--- src/Expense-item/ExpenseItem.js
+++ src/Expense-item/ExpenseItem.js
@@ -1,13 +1,15 @@
+import { ExpenseDate } from './components/Expense-date/ExpenseDate'
 import './ExpenseItem.css'
 
-export const ExpenseItem= ()=>{
+export const ExpenseItem= ({expense})=>{
+  
     return (
         <div className='expense-item'>
+            <ExpenseDate date={expense.date}></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div>Date</div>
+                <h2>{expense.title}</h2>
+            <div className='expense-item__price'>${expense.amount}</div>
             </div>
-            <div className='expense-item__price'>20$</div>
         </div>
     )
 }
---
+++ src/Expense-item/components/Expense-date/ExpenseDate.js
@@ -0,0 +1,15 @@
+import './ExpenseDate.css'
+
+export const ExpenseDate= ({date})=>{
+    const month = date.toLocaleString('en-US', {month:'long'})
+    const day = date.toLocaleString('en-US', {day:'2-digit'})
+    const year = date.getFullYear()
+    return(
+    <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+    </div>
+    )
+
+}
